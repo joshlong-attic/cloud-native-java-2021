819a90de8c9c35f3ed86d754168a7615a652fc08
Josh Long <josh@joshlong.com>
2020-09-27 13:22:26 -0700

    bootiful k8s

---
+++ bootiful-k8s/src/test/kotlin/com/example/bootifulk8s/BootifulK8sApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bootifulk8s
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootifulK8sApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
